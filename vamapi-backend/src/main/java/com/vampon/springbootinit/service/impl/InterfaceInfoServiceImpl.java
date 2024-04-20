a5f6673681e04c644701e18e7eaee078e2f1e9d4
403500543@qq.com <403500543@qq.com>
2024-04-20 17:36:20 +0800

    Added Redis cache for interface information query

--- vamapi-backend/src/main/java/com/vampon/springbootinit/service/impl/InterfaceInfoServiceImpl.java
+++ vamapi-backend/src/main/java/com/vampon/springbootinit/service/impl/InterfaceInfoServiceImpl.java
@@ -1,26 +1,40 @@
 package com.vampon.springbootinit.service.impl;
 
+import cn.hutool.core.util.ObjUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.vampon.springbootinit.common.BaseResponse;
 import com.vampon.springbootinit.common.ErrorCode;
+import com.vampon.springbootinit.constant.CommonConstant;
 import com.vampon.springbootinit.exception.BusinessException;
 import com.vampon.springbootinit.exception.ThrowUtils;
 import com.vampon.springbootinit.mapper.InterfaceInfoMapper;
 import com.vampon.springbootinit.model.dto.interfaceinfo.InterfaceInfoInvokeRequest;
+import com.vampon.springbootinit.model.dto.interfaceinfo.InterfaceInfoQueryRequest;
 import com.vampon.springbootinit.service.InterfaceInfoService;
 import com.vampon.springbootinit.service.UserService;
 import com.vampon.springbootinit.utils.VamApiClient;
 import com.vampon.vamapicommon.model.entity.InterfaceInfo;
 import com.vampon.vamapicommon.model.entity.User;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.vampon.springbootinit.constant.RedisConstant.*;
 
 /**
 * @author Fang Hao
@@ -33,9 +47,11 @@ public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, I
     @Resource
     private UserService userService;
 
-
     @Resource
     private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean add) {
@@ -53,6 +69,90 @@ public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, I
         if (StringUtils.isNotBlank(name) && name.length() > 1024) {
             throw new BusinessException(ErrorCode.PARAMS_ERROR , "内容过长");
         }
+    }
+
+    @Override
+    public Page<InterfaceInfo> listInterfaceInfoByPage(InterfaceInfoQueryRequest interfaceInfoQueryRequest) {
+        InterfaceInfo interfaceInfoQuery = new InterfaceInfo();
+        BeanUtils.copyProperties(interfaceInfoQueryRequest, interfaceInfoQuery);
+        long current = interfaceInfoQueryRequest.getCurrent();
+        long size = interfaceInfoQueryRequest.getPageSize();
+        String sortField = interfaceInfoQueryRequest.getSortField();
+        String sortOrder = interfaceInfoQueryRequest.getSortOrder();
+        String description = interfaceInfoQuery.getDescription();
+        String method = interfaceInfoQueryRequest.getMethod();
+        String name = interfaceInfoQueryRequest.getName();
+        // description 需支持模糊搜索
+        interfaceInfoQuery.setDescription(null);
+        // 限制爬虫
+        if (size > 50) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 从缓存中获取接口分页信息
+        // 计算 offset 和 count
+        long offset = (current - 1) * size;
+        long count = size;
+        // 获取redis中的数据
+        Set<String> allInterfacesInRedis = stringRedisTemplate.opsForZSet().reverseRange(CACHE_INTERFACEINFO_ALL_KEY, 0, -1);
+        Page<InterfaceInfo> page = new Page<>(current, size);
+        if(ObjUtil.isNotEmpty(allInterfacesInRedis)){
+            //缓存命中
+            // 对元素进行分页处理
+            List<InterfaceInfo> pagedInterfaces = allInterfacesInRedis.stream()
+                    .skip(offset)
+                    .limit(count)
+                    .filter(interfaceInRedis ->{
+                                InterfaceInfo interfaceInfo = this.convertJSONToInterfaceInfo(interfaceInRedis);
+                                // 查询条件
+                                if(StrUtil.isAllBlank(method,name,description)){
+                                    return true;
+                                }
+                                if(StrUtil.isNotBlank(method)){
+                                    if(interfaceInfo.getMethod().contains(method)){
+                                        return true;
+                                    }
+                                }
+                                if(StrUtil.isNotBlank(name)){
+                                    if(interfaceInfo.getName().contains(name)){
+                                        return true;
+                                    }
+                                }
+                                if(StrUtil.isNotBlank(description)){
+                                    if(interfaceInfo.getDescription().contains(description)){
+                                        return true;
+                                    }
+                                }
+                                return false;
+                            }
+                    )
+                    .map(this::convertJSONToInterfaceInfo)
+                    .collect(Collectors.toList());
+            page.setRecords(pagedInterfaces);
+            page.setTotal(allInterfacesInRedis.size());
+            System.out.println("缓存命中");
+            return page;
+        }
+        System.out.println("缓存未命中");
+        // 未命中,查询数据库
+        List<InterfaceInfo> allInterfaceInfoList = list();
+        page.setRecords(allInterfaceInfoList);
+        page.setTotal(allInterfaceInfoList.size());
+        //存储数据到redis中
+        for (InterfaceInfo interfaceInfo : page.getRecords()) {
+            // 将 InterfaceInfo 转为 JSON 字符串
+            String json = convertInterfaceInfoToJSON(interfaceInfo);
+
+            // 添加到 Redis 的 ZSet 中，使用默认的 score（即按照插入顺序）
+            stringRedisTemplate.opsForZSet().add(CACHE_INTERFACEINFO_ALL_KEY, json,interfaceInfo.getId());
+        }
+        return page;
+
+        //todo: queryWrapper需要再看看
+//        QueryWrapper<InterfaceInfo> queryWrapper = new QueryWrapper<>(interfaceInfoQuery);
+//        queryWrapper.like(StringUtils.isNotBlank(description), "description", description);
+//        queryWrapper.orderBy(StringUtils.isNotBlank(sortField),
+//                sortOrder.equals(CommonConstant.SORT_ORDER_ASC), sortField);
+//        Page<InterfaceInfo> interfaceInfoPage = this.page(new Page<>(current, size), queryWrapper);
     }
 
     @Override
@@ -115,6 +215,24 @@ public class InterfaceInfoServiceImpl extends ServiceImpl<InterfaceInfoMapper, I
             return invokeResult;
         }
     }
+
+    private String convertInterfaceInfoToJSON(InterfaceInfo interfaceInfo) {
+        return JSON.toJSONString(interfaceInfo);
+    }
+    private InterfaceInfo convertJSONToInterfaceInfo(String interfaceInfoJSON) {
+        return JSON.parseObject(interfaceInfoJSON, InterfaceInfo.class);
+    }
+
+    @Override
+    public void deleteRedisCache(Long id){
+        stringRedisTemplate.delete(CACHE_INTERFACEINFO_ALL_KEY);
+        stringRedisTemplate.delete(CACHE_INTERFACEINFO_KEY + id);
+    }
+
+//    @Override
+//    public Long getInterfaceInfoTotalInvokesCount() {
+//        return interfaceInfoMapper.getInterfaceInfoTotalInvokesCount().longValue();
+//    }
 
 }
 
