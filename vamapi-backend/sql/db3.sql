b590a9ceca5c3c0c42e0ea1ea8beae53328c5661
403500543@qq.com <403500543@qq.com>
2024-07-14 10:06:38 +0800

    增加了接口信息统计（接口接入数量，最近接口调用响应时延，接口调用次数，网站访问次数），同时增加了API接口文档。修复了部分智能接口的超时bug，新增了几个新的接口。同时改变了用户获取AK/SK凭证的途径（改为下载文件且智能下载一次，第二次需要直接更新凭证）

--- vamapi-backend/sql/db3.sql
+++ vamapi-backend/sql/db3.sql
@@ -1,15 +1,29 @@
 use vamapi;
 
--- 用户凭证下载表
-create table if not exists user_voucherUrl_info
-(
-    id          bigint auto_increment comment 'id' primary key,
-    userId      bigint                             not null comment '用户id',
-    voucherUrl  varchar(256)                       not null comment '凭证下载链接',
-    downloadStatus     tinyint                           not null comment '凭证下载状态（0-未下载，1-已下载）',
-    downloadTime datetime                         null comment '下载时间',
-    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete   tinyint  default 0                 not null comment '是否删除'
-)
-    comment '用户凭证下载表';
+# -- 用户凭证下载表
+# create table if not exists user_voucherUrl_info
+# (
+#     id          bigint auto_increment comment 'id' primary key,
+#     userId      bigint                             not null comment '用户id',
+#     voucherUrl  varchar(256)                       not null comment '凭证下载链接',
+#     downloadStatus     tinyint                           not null comment '凭证下载状态（0-未下载，1-已下载）',
+#     downloadTime datetime                         null comment '下载时间',
+#     createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+#     updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+#     isDelete   tinyint  default 0                 not null comment '是否删除'
+# )
+#     comment '用户凭证下载表';
+
+-- 查询用户近一周内每天注册信息
+SELECT DATE(createTime) AS create_date, COUNT(*) AS user_count
+FROM user
+WHERE createTime >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+GROUP BY DATE(createTime)
+ORDER BY create_date;
+
+-- 查询用户近一周内每天接口调用次数
+SELECT DATE(createTime) AS create_date, COUNT(*) AS invoke_count, AVG(requestDuration) as duration
+FROM interface_log
+WHERE createTime >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+GROUP BY DATE(createTime)
+ORDER BY create_date;
